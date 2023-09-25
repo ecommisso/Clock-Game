70c3e4c358b956e02e0f0c415c7c622fad724ab9
Aparna-Rajesh <aparnarajesh2008@gmail.com>
2023-09-24 23:51:33 -0400

    adding counter val to avoid relying on alphabetization to play a factor when tie-breaking

--- players/team_8.py
+++ players/team_8.py
@@ -97,7 +97,7 @@ class Player:
                            * 0.4) + alternating_sbscore * 0.3 + doubles_sbscore * 0.3
 
             heapq.heappush(constraint_heap, (const_score * -1,
-                           len(lst) * -1, next(counter), constraint))  # heap prioritizes by score, then by length, then by alphabetization of constraints
+                           len(lst) * -1, next(counter), constraint))  # heap prioritizes by score, then by length, then by counter (do not want to rely on alphabetization of constraints as a tie-breaker)
 
             if data_mode:
                 with open("data.txt", "a") as file1:
