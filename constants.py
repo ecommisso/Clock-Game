e67eb35ffa81ae9a2254e6c2bd9aad06169127df
cmartema <cjm2301@columbia.edu>
2023-09-25 11:49:24 -0400

    Co-authored-by: mariarice15 <mariarice15@users.noreply.github.com> Co-authored-by: Anita Bui <anitabui@users.noreply.github.com>

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 import os
 import argparse 
 
-timeout = 100000 # 10s aggregate time limit
-number_of_constraints_pp = 5
+timeout = 10 # 10s aggregate time limit
+number_of_constraints_pp = 10
 c = 5
 exact_pos = 20
 rng_seed = 5
