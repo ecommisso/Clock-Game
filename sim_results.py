fcddc93ba43edeb996c5413cf4727e29fcea8044
Ken Miura <km3635@columbia.edu>
2023-09-26 01:43:36 -0400

    fixed stddev output in sim

--- sim_results.py
+++ sim_results.py
@@ -7,9 +7,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('vals', type=int, nargs='+')
     args = parser.parse_args().vals
-    score, varsum, n = args[:3], args[3:-1], args[-1]
-    avg = [s/n for s in score]
-    stddev = [sqrt(v/n) for v in varsum]
+    n = args[-1]
+    scores = [[],[],[]]
+    for i, s in enumerate(args[:-1]):
+        scores[i%3].append(s)
+    for s in scores:
+        print(s)
+    avg = [sum(s)/n for s in scores]
+    stddev = [sqrt(sum(map(lambda x: (x - avg[i])**2, s)) / n) for i, s in enumerate(scores)]
 
     for i in range(3):
         print(f'P{i+1}: mean = {avg[i]} ; stddev = {stddev[i]}')
--- simulate.sh
+++ simulate.sh
@@ -8,22 +8,17 @@ p2=$3
 p3=$4
 
 #don't touch these
-scores=(0 0 0)
-variance=(0 0 0)
+s1=()
+s2=()
+s3=()
 
 for ((i=0 ; i < n ; i++)); do
 	printf "$p1\n$p2\n$p3\n" | bash clock_game.sh True $RANDOM &&
 		sleep 1
-	sed -n '26,28 p' log_moves.txt | awk '{print $5}' > tmp
-	mapfile -t temp < tmp
-	for j in {0..2}; do
-		t=${temp[${j}]}
-		((scores[j]+=t))
-		((variance[j]+=t*t))
-	done
+	sed -n '26,28 p' log_moves.txt | awk '{print $5}' >> tmp
 done
+readarray -t temp < tmp
 rm tmp
-echo "${scores[@]}"
-echo "${variance[@]}"
-python sim_results.py "${scores[@]}" "${variance[@]}" "$n"
+echo "${temp[@]}"
+python sim_results.py ${temp[@]} $n
 
