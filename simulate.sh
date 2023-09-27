ab0fb26ba2697754f9ba14b23f1bb63766037f7c
Ken Miura <km3635@columbia.edu>
2023-09-26 22:47:36 -0400

    fixed to work with new log output

--- simulate.sh
+++ simulate.sh
@@ -6,11 +6,11 @@ p1=$2
 p2=$3
 p3=$4
 
-for i in `seq 0 $n`
+for i in `seq 1 $n`
 do
 	printf "$p1\n$p2\n$p3\n" | bash clock_game.sh True $RANDOM &&
 		sleep 1
-	sed -n '26,28 p' log_moves.txt | awk '{print $5}' >> tmp
+	sed -n -e '26p' -e '28p' -e '30p' log_moves.txt | awk '{print $5}' >> tmp
 done
 readarray -t temp < tmp
 rm tmp
